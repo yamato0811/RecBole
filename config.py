3d046de61cae952786ba7146a67bb3ed79320fcf
yamato0811 <yama8to11@gmail.com>
2022-01-27 16:13:37 +0900

    make config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+from recbole.config import Config
+from options import get_args
+
+args = get_args()
+
+# configurations initialization
+def get_config():
+    config_dict = {
+        'gpu_id' : '1',
+        'epochs' : 1,
+        'loss_type' : 'CE',
+        'neg_sampling' : 'None',
+        'MAX_ITEM_LIST_LENGTH' : f'{args.max_len}',
+
+        'metrics' : ['Recall', 'NDCG'],
+        'valid_metric': 'NDCG@10',
+        'topk' : [1,5,10,20,50],
+        'save_dataset' : True,
+        'load_col' : {'inter': ['user_id', 'item_id', 'rating', 'timestamp']},
+        'eval_args' : {'split': {'LS': 'test_only'}, 'order': 'TO', 'mode': 'uni100', 'group_by': 'user'}
+    }
+
+    config = Config(model=args.model, dataset=args.dataset, config_dict=config_dict, config_file_list=[f'dataset/{args.dataset}/{args.dataset}.yaml'])
+    return config
